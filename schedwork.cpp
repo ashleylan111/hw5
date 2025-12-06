fab9bdf061897ea82a75eb0a1a3db187c2a94f89
Ashley Lan <lanashle@usc.edu>
2025-12-06 04:42:06 +0000

    Implementation finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -17,14 +17,76 @@ using namespace std;
 // a constant that can be used to indicate an INVALID 
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
-static const Worker_T INVALID_ID = (unsigned int)-1;
+static const Worker_T INVALID_ID = (unsigned int) - 1;
 
 
 // Add prototypes for any helper functions here
 
+bool schedHelper(const AvailabilityMatrix& avail, 
+size_t dailyNeed, size_t maxShifts, 
+DailySchedule& sched, vector<size_t>& shiftCount, 
+size_t day, size_t slot){
+
+  size_t n = avail.size();
+  size_t k = avail[0].size();
+
+  if (day == n){
+    return true;
+  }
+
+  if (slot == dailyNeed){
+    return schedHelper(avail, dailyNeed, maxShifts, sched, shiftCount, day + 1, 0);
+  }
+
+  for (size_t w = 0; w < k; w++){
+    if (!avail[day][w]){
+      continue;
+    }
+
+    if (shiftCount[w] >= maxShifts){
+      continue;
+    }
+
+    if (find(sched[day].begin(), sched[day].begin() + slot, w) != sched[day].begin() + slot){
+      continue;
+    }
+
+    sched[day][slot] = w;
+    shiftCount[w]++;
+
+    if (schedHelper(avail, dailyNeed, maxShifts, sched, shiftCount, day, slot + 1)){
+      return true;
+    }
+
+    shiftCount[w]--;
+
+    
+  }
+
+  return false; 
+
+  
+}
+
 
 // Add your implementation of schedule() and other helper functions here
-
+/**
+ * @brief Produces a work schedule given worker availability,
+ *        the number of needed workers per day, and the maximum 
+ *        shifts any single worker is allowed. Returns true
+ *        and the valid schedule if a solution exists, and false
+ *        otherwise. 
+ * 
+ * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
+ *                    of the k workers for each of the n days
+ * @param [in]  dailyNeed Number of workers needed per day (aka d)
+ * @param [in]  maxShifts Maximum shifts any worker is allowed over 
+ *                        the n day period (aka m)
+ * @param [out] sched n x d vector of vectors indicating the d workers
+ *                    who are scheduled to work on each of the n days
+ * @return true if a solution exists; sched contains the solution
+ * @return false if no solution exists; sched is undefined (can be anything)
+ */
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -36,10 +98,16 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    // Add your code below 
 
+    size_t n = avail.size();
+    size_t k = avail[0].size();
 
+    sched = DailySchedule(n, vector<Worker_T>(dailyNeed));
 
+    vector<size_t> shiftCount(k, 0);
+
+    return schedHelper(avail, dailyNeed, maxShifts, sched, shiftCount, 0, 0);
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -12,9 +12,25 @@
 using namespace std;
 
 
+
 // Add prototypes of helper functions here
 
+void wordFinder(const std::string current,
+    std::string floating,
+    const std::set<std::string>& dict, 
+    std::set<std::string>& results);
 
+/**
+ * @brief Returns the list of all legal words in the provided dictionary
+ *        that meet the criteria provided in the `in` and `floating`
+ *        inputs.
+ * 
+ * @param [in] in - string of fixed, correct characters and `-`s to indicate
+ *                  the length of string and locations that must be filled in
+ * @param [in] floating  - Characters that must be used somewhere in the word
+ * @param [in] dict - Dictionary of strings of legal words
+ * @return std::set<std::string> - Set of all words that meet the criteria
+ */
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -23,6 +39,70 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::set<std::string> results;
+
+    std::string current = in;
+    wordFinder(current, floating, dict, results);
+
+    return results;
+
 }
 
 // Define any helper functions here
+
+void wordFinder(const std::string current,
+    const std::string floating,
+    const std::set<std::string>& dict, 
+    std::set<std::string>& results){
+
+    //Base Case: If there are no blanks 
+    if (current.find('-') == std::string::npos){
+
+        if (floating.empty() && dict.count(current)){
+            results.insert(current);
+        }
+
+        return;
+        
+    } 
+
+    int blank_left = std::count(current.begin(), current.end(), '-');
+    if (floating.size() > blank_left){
+        return;
+    }
+
+    int idx = current.find('-');
+
+    //Let the floating letters cover the blanks first 
+
+    for (int i = 0; i < floating.size(); i++){
+        char letter = floating[i];
+
+        std::string nextWord = current;
+        nextWord[idx] = letter;
+        std::string nextFloating = floating;
+        nextFloating.erase(i, 1); //Removing the float letter after attempted try
+
+        wordFinder(nextWord, nextFloating, dict, results);
+
+    }
+
+    //Then let the rest of the blanks fill up 
+
+    if (floating.size() <= blank_left - 1){
+        for (char x = 'a'; x <= 'z'; x++){
+
+            if (floating.find(x) != std::string::npos){
+                continue;
+            }
+
+            std::string nextWord = current;
+            nextWord[idx] = x;
+
+            wordFinder(nextWord, floating, dict, results);
+
+        }
+    }
+
+
+}
